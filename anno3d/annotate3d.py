565e7b634c02004efabf2021455ee5827945daa5
YuigaWada <yuigawada@gmail.com>
2024-02-11 04:59:30 +0900

    wip

--- anno3d/annotate3d.py
+++ anno3d/annotate3d.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from pathlib import Path
 from tqdm import tqdm
+import viser.transforms as vtf
 
 class GUIEditor:
     def __init__(self,with_texture=True) -> None:
@@ -17,21 +18,43 @@ class GUIEditor:
         mesh = o3d.io.read_triangle_mesh(obj_path, enable_post_processing=self.with_texture)
         assert mesh.textures
 
-        picked_indices = []
-        while len(picked_indices) < 2:
-            picked_points = self._pick_points(mesh)
-            picked_indices = [pp.coord for pp in picked_points]
+        nerf_to_open3d = np.array([
+            [1, 0, 0, 0],
+            [0, 0, -1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 1]
+        ])
+        transform_matrix = np.array([
+[-0.5614292801786869, 0.8268592979224536, -0.03317928265090797, 0.5195994973182678],
+[-0.8257877680585626, -0.5623967795941137, -0.04224244816565299, 8.050601959228516],
+[-0.05358848274489096, 0.00368289849944943, 0.9985563132721876, 1.0177491903305054],
+[0.0, 0.0, 0.0, 1.0],
+        ])
+        c2w = transform_matrix
+        R = vtf.SO3.from_matrix(c2w[:3, :3])
+        # R = R @ vtf.SO3.from_x_radians(np.pi)
+        transform_matrix[:3,:3] = R.as_matrix()
+        # transform_matrix[:3,:3] = (vtf.SO3.from_matrix(np.eye(3)) @ vtf.SO3.from_x_radians(np.pi)).as_matrix()
+        
+        transform_matrix[:3,3] /= 10
+        self.coordinate_frame.transform(transform_matrix)
+
+        # picked_indices = []
+        # while len(picked_indices) < 2:
+        #     picked_points = self._pick_points(mesh)
+        #     picked_indices = [pp.coord for pp in picked_points]
 
         # clastering
-        vertices = np.asarray(picked_indices)
-        kmeans = KMeans(n_clusters=2)
-        kmeans.fit(vertices)
-        centroids = kmeans.cluster_centers_
-
+        # vertices = np.asarray(picked_indices)
+        # kmeans = KMeans(n_clusters=2)
+        # kmeans.fit(vertices)
+        centroids = np.array([[0.01026475,-0.04470124,-0.24673722],
+                            [0.0046543,-0.05070926,-0.3248854]])
         # visualize
         vis = o3d.visualization.Visualizer()
-        vis.create_window()
+        vis.create_window(width=1080, height=1920)
         cylinder = self._create_cylinder_between_points(centroids[0], centroids[1], color=[1, 0, 0])
+        cylinder.transform(transform_matrix)
         vis.add_geometry(cylinder)
         vis.add_geometry(mesh)
         vis.add_geometry(self.coordinate_frame)
@@ -71,7 +94,7 @@ class GUIEditor:
     
 class Annotator:
     def __init__(self) -> None:
-        editor = GUIEditor(with_texture=False)
+        editor = GUIEditor(with_texture=True)
         self.editor = editor
 
     def run(self):
@@ -101,8 +124,6 @@ class Annotator:
         yaw = math.atan2(y, x)
         roll = 0
         return [math.degrees(x) for x in [yaw, pitch, roll]]
-
-
 
 def main():
     annotator = Annotator()
